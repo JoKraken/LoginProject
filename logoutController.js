ab70423fdd113a5b22701af66a209822cf62c0b5
Jojo <J.Kraken24@hotmail.com>
2021-06-26 17:39:16 +0200

    edit stay login - function

--- logoutController.js
+++ logoutController.js
@@ -1,9 +1,27 @@
 var app = angular.module('myApp', []);
 app.controller('logoutCtrl', function($scope) {
-    if(!JSON.parse(localStorage.getItem("login")).stay)
+    let login = JSON.parse(localStorage.getItem("login"));
+    if(login.stay){
+        if(login.num == undefined) {
+            login.num = 1;
+            localStorage.setItem("login", JSON.stringify(login));
+        } else if(login.num < 5) {
+            login.num++;
+            localStorage.setItem("login", JSON.stringify(login));
+        } else {
+            login.stay = false;
+            login.num = undefined;
+            localStorage.setItem("login", JSON.stringify(login));
+            window.location.href = "./index.html";
+        }
+    } else if(!login.stay && login.num != undefined)
         window.location.href = "./index.html";
+    else {
+        login.num = 1;
+        localStorage.setItem("login", JSON.stringify(login));
+    }
     $scope.data = {}
-    let id = JSON.parse(localStorage.getItem("login")).id;
+    let id = login.id;
     let list = JSON.parse(localStorage.getItem("list"));
     for(let i = 0; i < list.length; i++){
         if(list[i].id == id) {
